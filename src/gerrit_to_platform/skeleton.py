36fe59db2f77f7312be3425e6878a1e59d7fa59e
Andrew Grimberg <agrimberg@linuxfoundation.org>
2023-02-08 11:50:59 -0800

    CI: Add additional tox test
    * Validate license headers (and add them to the boilerplate files)
    * Run pre-commit validation in tox so CI can validate it
    
    Change-Id: Ib92090d1506dabbd3c547166a4b86c74a0f9326a
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- src/gerrit_to_platform/skeleton.py
+++ src/gerrit_to_platform/skeleton.py
@@ -1,3 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2023 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the Apache-2.0 license which accompanies this
+# distribution, and is available at
+# https://opensource.org/licenses/Apache-2.0
+##############################################################################
 """
 This is a skeleton file that can serve as a starting point for a Python
 console script. To run this script uncomment the following lines in the
