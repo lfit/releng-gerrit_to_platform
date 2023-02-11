aaed442802acea58a37a68b2beff995d261b5664
Andrew Grimberg <agrimberg@linuxfoundation.org>
2023-02-11 08:49:22 -0800

    Feat: Add stubs for all hook types being supported
    Issue: RELENG-4564
    Change-Id: I1d062e7f8992ad6a6e381cf43ec1ae8b7f67ddfe
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

---
+++ src/gerrit_to_platform/cli/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2023 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials are made
+# available under the terms of the Apache-2.0 license which accompanies this
+# distribution, and is available at
+# https://opensource.org/licenses/Apache-2.0
+##############################################################################
+"""CLI main for gerrit_to_platform."""
