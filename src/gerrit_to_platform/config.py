e5b88c31969da0e57f1a32c90b02e515492dac74
Andrew Grimberg <agrimberg@linuxfoundation.org>
2023-02-23 06:26:44 -0800

    Feat: Add import of gerrit replication remotes
    Change-Id: Ifa71f64ad20f3f6bd8866f499bfbd6a8e813a3d6
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- src/gerrit_to_platform/config.py
+++ src/gerrit_to_platform/config.py
@@ -11,22 +11,118 @@
 
 import configparser
 import os.path
-from configparser import ConfigParser
-from typing import Optional, Union
+import re
+from configparser import ConfigParser, NoOptionError
+from enum import Enum
+from typing import Dict, Optional, TypedDict, Union
 
 from xdg import XDG_CONFIG_HOME
+
+# CONSTANTS
+CONFIG = "config"
+REPLICATION = "replication"
+DEFAULT_CONFIG = CONFIG
+
+
+# Type Definitions
+class Remote(TypedDict):
+    """Remote Dictionary definition."""
+
+    owner: str
+    remotenamestyle: str
+    repo: str
+
+
+PlatformType = Dict[str, Remote]
+
+
+class ReplicationRemotes(TypedDict, total=False):
+    """Platform Dictionary definition."""
+
+    github: PlatformType
+    gitlab: PlatformType
+
+
+class Platform(Enum):
+    GITHUB = "github"
+    GITLAB = "gitlab"
+
 
 G2P_CONFIG_FILE = os.path.join(
     XDG_CONFIG_HOME, "gerrit_to_platform", "gerrit_to_platform.ini"
 )
 
+GERRIT_REPLICATION_CONFIG = os.path.join(
+    XDG_CONFIG_HOME, "gerrit_to_platform", "replication.config"
+)
 
-def get_config() -> ConfigParser:
+CONFIG_FILES = {
+    CONFIG: G2P_CONFIG_FILE,
+    REPLICATION: GERRIT_REPLICATION_CONFIG,
+}
+
+
+def get_config(config_type: str = DEFAULT_CONFIG) -> ConfigParser:
     """Get the config object."""
     config = configparser.ConfigParser()
-    with open(G2P_CONFIG_FILE) as config_file:
-        config.read_file(config_file, G2P_CONFIG_FILE)
+    conf_file = CONFIG_FILES[config_type]
+    with open(conf_file) as config_file:
+        config.read_file(config_file, conf_file)
     return config
+
+
+def get_replication_remotes() -> ReplicationRemotes:
+    """Get the replication remotes available."""
+    remotes_config = get_config(REPLICATION)
+    remotes: ReplicationRemotes = {}
+
+    for section in remotes_config.sections():
+        subsect = r'"(.*)"'
+        subsection = re.findall(subsect, section)[0]
+        url = remotes_config.get(section, "url")
+        try:
+            authgroup = remotes_config.get(section, "authgroup")
+        except NoOptionError:
+            authgroup = ""
+        try:
+            namestyle = remotes_config.get(section, "remotenamestyle")
+        except NoOptionError:
+            namestyle = "slash"
+
+        if (
+            "github" in section.lower()
+            or "github" in subsection.lower()
+            or "github" in authgroup.lower()
+        ):
+            platform_type = Platform.GITHUB.value
+        elif (
+            "gitlab" in section.lower()
+            or "gitlab" in subsection.lower()
+            or "gitlab" in authgroup.lower()
+        ):
+            platform_type = Platform.GITLAB.value
+        else:
+            continue
+
+        url_regex = r":(.*)/(.*)$"
+        owner, repo = re.findall(url_regex, url)[0]
+
+        if platform_type in remotes.keys():
+            remotes[platform_type][subsection] = {  # type: ignore
+                "owner": owner,
+                "remotenamestyle": namestyle,
+                "repo": repo,
+            }
+        else:
+            remotes[platform_type] = {  # type: ignore
+                subsection: {
+                    "owner": owner,
+                    "remotenamestyle": namestyle,
+                    "repo": repo,
+                }
+            }
+
+    return remotes
 
 
 def has_section(section: str) -> bool:
