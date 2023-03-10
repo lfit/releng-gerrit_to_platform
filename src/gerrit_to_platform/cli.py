64d313ef38827205e6c06e0bca8369768d9bb2dd
Andrew Grimberg <agrimberg@linuxfoundation.org>
2023-03-10 13:43:54 -0800

    Refactor: Move change_merged to it's own module
    Issue: RELENG-4564
    Change-Id: I07b47d6869c60941388d775bc9abd64176c0f290
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- src/gerrit_to_platform/cli.py
+++ src/gerrit_to_platform/cli.py
@@ -15,29 +15,14 @@ import typer
 
 import gerrit_to_platform.comment_added as comment_added
 import gerrit_to_platform.patchset_created as patchset_created
+from gerrit_to_platform.change_merged import app as change_merged
 
 app = typer.Typer()
 app.add_typer(
     patchset_created.app, name="patchset-created", help="Handle patchset-created hook"
 )
 app.add_typer(comment_added.app, name="comment-added", help="Handle comment-added hook")
-
-
-@app.command()
-def change_merged(
-    change: str = typer.Option(..., help="change id"),
-    change_url: str = typer.Option(..., help="change url"),
-    change_owner: str = typer.Option(..., help="change owner"),
-    change_owner_username: str = typer.Option(..., help="username"),
-    project: str = typer.Option(..., help="project name"),
-    branch: str = typer.Option(..., help="branch"),
-    topic: str = typer.Option(..., help="topic"),
-    submitter: str = typer.Option(..., help="submitter"),
-    submitter_username: str = typer.Option(..., help="username"),
-    commit: str = typer.Option(..., help="sha1"),
-    newrev: str = typer.Option(..., help="sha1"),
-):
-    """Handle change-merged hook."""
+app.add_typer(change_merged, name="change-merged", help="Handle change-merged hook")
 
 
 @app.command()
