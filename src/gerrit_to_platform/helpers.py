9345cf11b8657838149a7e86d99f274bf3fffd0a
Andrew Grimberg <agrimberg@linuxfoundation.org>
2023-03-11 15:29:28 -0800

    Feat: Create find_and_dispatch helper
    patchset-created, comment-added, and change-merged all use essentially
    the same logic for finding and dispatching the workflows involved. Make
    a helper that the hooks can all use to deduplicate work.
    
    Change-Id: I5a053f392c5c5dc14e75d8f4bcd591ca77d1b249
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- src/gerrit_to_platform/helpers.py
+++ src/gerrit_to_platform/helpers.py
@@ -10,10 +10,14 @@
 """Common helper functions."""
 
 import re
-from typing import Callable, Union
+from typing import Callable, Dict, Union
 
 import gerrit_to_platform.github as github
-from gerrit_to_platform.config import Platform, ReplicationRemotes
+from gerrit_to_platform.config import (
+    Platform,
+    ReplicationRemotes,
+    get_replication_remotes,
+)
 
 
 def choose_dispatch(platform: Platform) -> Union[Callable, None]:
@@ -50,6 +54,42 @@ def convert_repo_name(
     converted_repository = repository.replace("/", remote_styles[target_style])
 
     return converted_repository
+
+
+def find_and_dispatch(project: str, workflow_filter: str, inputs: Dict[str, str]):
+    """Find relevant workflows and dispatch them."""
+    remotes = get_replication_remotes()
+
+    for platform in Platform:
+        if platform.value not in remotes:
+            continue
+
+        dispatcher = choose_dispatch(platform)
+        filter_workflows = choose_filter_workflows(platform)
+
+        if dispatcher is None or filter_workflows is None:
+            continue
+
+        for remote in remotes[platform.value]:
+            owner = remotes[platform.value][remote]["owner"]
+            repo = convert_repo_name(remotes, platform, remote, project)
+            workflows = filter_workflows(owner, repo, workflow_filter)
+
+            for workflow in workflows:
+                print(
+                    f"Dispatching workflow '{workflow['name']}', "
+                    + f"id {workflow['id']} on "
+                    + f"{platform.value}:{owner}/{repo} for change "
+                    + f"{inputs['GERRIT_CHANGE_NUMBER']} patch "
+                    + inputs["GERRIT_PATCHSET_NUMBER"]
+                )
+                dispatcher(
+                    owner,
+                    repo,
+                    workflow["id"],
+                    f"refs/heads/{inputs['GERRIT_BRANCH']}",
+                    inputs,
+                )
 
 
 def get_change_id(change: str) -> str:
